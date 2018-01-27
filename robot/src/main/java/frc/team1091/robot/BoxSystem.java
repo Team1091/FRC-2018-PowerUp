444069c3818d83f549e59b47994fb90254d33e28
Alyssa Lear <learaly@huhs.org>
2018-01-27 13:50:29 -0600

    Refactored the Box System and Started the Gate System

--- robot/src/main/java/frc/team1091/robot/BoxSystem.java
+++ robot/src/main/java/frc/team1091/robot/BoxSystem.java
@@ -3,32 +3,79 @@ package frc.team1091.robot;
 public class BoxSystem {
     private final double stopBoxIngestionAtValue = 3;
     private RobotComponents robotComponents;
+
+    private BoxConsumptionState currentState;
+
     public BoxSystem (RobotComponents robotComponents) {
+        currentState = BoxConsumptionState.Idle;
         this.robotComponents = robotComponents;
     }
     public void ingestBox(){
-        //tell if box is present (ultrasonic, use mv to mm equation on the package)
-        int averageRaw = robotComponents.ultraSonicAnalogInput.getAverageValue();
-        boolean suckerSucking = robotComponents.suckerMotor.get() >= 0;
-        boolean boxIngested = averageRaw >= stopBoxIngestionAtValue;
-        if (boxIngested && !suckerSucking) return;
-        if (robotComponents.pickUpPositionDigitalInput.get()) {
-            if (!boxIngested){
-                robotComponents.suckerMotor.set(1);
-            }else{
-                robotComponents.suckerMotor.set(0);
-                while (robotComponents.gateClosePositionDigitalInput.get()){
-                    //lift gate to pen in the box
-                    robotComponents.gateMotor.set(.8);
+
+        switch (currentState) {
+            case Idle:
+                //tell if box is present (ultrasonic, use mv to mm equation on the package)
+                if (canIngestBox()) {
+                    startIngestingBox();
                 }
-                robotComponents.gateMotor.set(0);
-            }
+                break;
+            case IngestingBox:
+                if (isBoxIngested()) {
+                    finishedIngestingBox();
+                }
+                break;
+            case IngestedBox:
+                closeGate();
+                break;
+            case ClosingGate:
+                if (isGateClosed()){
+                    gateClosed();
+                }
+                break;
         }
-
-
         //throw box
 
         //lift the platform at an angle and bring to drop box
-
     }
+
+    public boolean isBoxIngested(){
+        int averageRaw = robotComponents.ultraSonicAnalogInput.getAverageValue();
+        boolean boxIngested = averageRaw >= stopBoxIngestionAtValue;
+        return boxIngested;
+    }
+    public boolean canIngestBox(){
+        boolean boxIngested = isBoxIngested();
+        boolean isInPickupPosition = robotComponents.pickUpPositionDigitalInput.get();
+        return !boxIngested && isInPickupPosition;
+    }
+    public void startIngestingBox (){
+        currentState = BoxConsumptionState.IngestingBox;
+        robotComponents.suckerMotor.set(1);
+    }
+    public void finishedIngestingBox (){
+        currentState = BoxConsumptionState.IngestedBox;
+        robotComponents.suckerMotor.set(0);
+    }
+    public void closeGate (){
+            currentState = BoxConsumptionState.ClosingGate;
+            //Keep moving motor to close
+            robotComponents.gateMotor.set(.8);
+    }
+    public void gateClosed (){
+        currentState = BoxConsumptionState.Idle;
+        //Stop moving motor
+        robotComponents.gateMotor.set(0);
+    }
+    public boolean isGateClosed (){
+        boolean gateIsClosed = robotComponents.gateClosePositionDigitalInput.get();
+        return gateIsClosed;
+    }
+
+}
+
+enum BoxConsumptionState {
+    Idle,
+    IngestingBox,
+    IngestedBox,
+    ClosingGate
 }
---
+++ robot/src/main/java/frc/team1091/robot/GateSystem.java
@@ -0,0 +1,38 @@
+package frc.team1091.robot;
+
+public class GateSystem {
+    private RobotComponents robotComponents;
+    private GateSystemState currentState;
+    public GateSystem (RobotComponents components){
+        robotComponents = components;
+    }
+    public void controlGate (){
+        boolean shouldMoveUp = robotComponents.xboxController.getRawAxis(Xbox.rt) > 0;
+        boolean shouldMoveDown = robotComponents.xboxController.getRawAxis(Xbox.lt) > 0;
+        switch (currentState){
+            case Idle:
+                if (shouldMoveUp) {
+                    // start moving gate up
+                    moveUp();
+                }
+                if (shouldMoveDown){
+                    // start moving gate down
+                }
+                break;
+            case MovingDown:
+                break;
+            case MovingUp:
+                break;
+        }
+    }
+    public void moveUp (){
+        currentState = GateSystemState.MovingUp;
+        robotComponents.gateMotor.set(1);
+    }
+}
+
+enum GateSystemState {
+    Idle,
+    MovingDown,
+    MovingUp
+}
