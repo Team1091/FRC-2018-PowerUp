39bd5c2253e732f1275cc4a00a8649ff9741aa24
moltenpopsicle <brennan.wolters@gmail.com>
2018-02-17 12:20:04 -0600

    Experimental elevator changes

--- robot/src/main/java/frc/team1091/robot/systems/ElevatorPositions.java
+++ robot/src/main/java/frc/team1091/robot/systems/ElevatorPositions.java
@@ -1,7 +1,14 @@
 package frc.team1091.robot.systems;
 
 public enum ElevatorPositions {
-    GROUND_HEIGHT,
-    SWITCH_HEIGHT,
-    SCALE_HEIGHT,
+
+    GROUND_HEIGHT(0.0),
+    SWITCH_HEIGHT(24.0),
+    SCALE_HEIGHT(36.0);
+
+    public final double inches;
+
+    ElevatorPositions(double inches) {
+        this.inches = inches;
+    }
 }
--- robot/src/main/java/frc/team1091/robot/systems/ElevatorSystem.java
+++ robot/src/main/java/frc/team1091/robot/systems/ElevatorSystem.java
@@ -1,48 +1,53 @@
 package frc.team1091.robot.systems;
 
-import edu.wpi.first.wpilibj.PIDController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.team1091.robot.RobotComponents;
 import frc.team1091.robot.Xbox;
 
 public class ElevatorSystem {
     private RobotComponents robotComponents;
-//12723 = 90 inches
+    //12723 = 90 inches
 //141.5 counts per inch
     private final double stepDownStartAt = .5;
-    private final int scaleHeight = 720;
-    private final int switchHeight = 360;
-    private final int switchRange = 12;
+    private final int switchRange = 2;
 
-    private final double decentSpeed = .3;
     final double throttledMotorSpeed = 0.7;
 
-    private double lastControlTime;
-    private double currentControlTime;
     private ElevatorPositions targetPosition = ElevatorPositions.GROUND_HEIGHT;
+
+    private double holdPosition = 0; // current desired height
+    private final double maxSpeed = 1; // max change in desired speed
 
     public ElevatorSystem(RobotComponents robotComponents) {
         this.robotComponents = robotComponents;
     }
 
     public void controlLift(double dt) {
-        currentControlTime = dt;
-        if(lastControlTime != 0) {
-            //Determine which way we need to go
-            setStateFromController();
-            switch (targetPosition) {
-                case GROUND_HEIGHT:
-                    goToGround();
-                    break;
-                case SWITCH_HEIGHT:
-                    goToSwitch();
-                    break;
-                case SCALE_HEIGHT:
-                    goToScale();
-                    break;
-            }
+        //Determine which way we need to go
+        setStateFromController();
+
+        // Update target
+        if (targetPosition.inches > holdPosition) {
+            holdPosition += maxSpeed * dt;
+        } else {
+            holdPosition -= maxSpeed * dt;
         }
-        lastControlTime = currentControlTime;
+
+        double actionMeasured = robotComponents.elevatorEncoder.getDistance();
+
+        double speed = determineMotorSpeed(actionMeasured, holdPosition);
+
+        if (isAtPosition(ElevatorPositions.GROUND_HEIGHT)) {
+            robotComponents.elevatorEncoder.reset();
+            robotComponents.elevatorMotor.set(0);
+            return;
+        }
+
+        if (holdPosition > actionMeasured) {// go up
+            robotComponents.elevatorMotor.set(speed);
+        } else {//go down
+            robotComponents.elevatorMotor.set(-speed);
+        }
+
     }
 
     public ElevatorPositions getTargetPosition() {
@@ -53,41 +58,22 @@ public class ElevatorSystem {
         targetPosition = desiredPosition;
     }
 
-    private void goToGround() {
-        if (isAtPosition(ElevatorPositions.GROUND_HEIGHT)) {
-            robotComponents.elevatorEncoder.reset();
-            robotComponents.elevatorMotor.set(0);
-            return;
-        }
-        //Slam it to the bottom
-        robotComponents.elevatorMotor.set(decentSpeed);
-    }
-
     public boolean isAtPosition(ElevatorPositions position) {
         double currentHeight = robotComponents.elevatorEncoder.getDistance();
-        switch (position){
+        switch (position) {
             case GROUND_HEIGHT:
-                return robotComponents.elevatorLimitSwitch.get();
+                return currentHeight <= 0;
             case SWITCH_HEIGHT:
-                double upperSwitchRange = switchHeight + switchRange;
-                double lowerSwitchRange = switchHeight - switchRange;
-                return !(currentHeight <= upperSwitchRange && currentHeight >= lowerSwitchRange);
             case SCALE_HEIGHT:
-                double upperScaleRange = scaleHeight + switchRange;
-                double lowerScaleRange = scaleHeight - switchRange;
-                return !(currentHeight <= upperScaleRange && currentHeight >= lowerScaleRange);
+                double upperSwitchRange = position.inches + switchRange;
+                double lowerSwitchRange = position.inches - switchRange;
+                return !(currentHeight <= upperSwitchRange && currentHeight >= lowerSwitchRange);
         }
         return false;
     }
 
     public boolean isAtDropPosition() {
-        if (targetPosition == ElevatorPositions.SCALE_HEIGHT && isAtPosition(ElevatorPositions.SCALE_HEIGHT)) {
-            return true;
-        }
-        if (targetPosition == ElevatorPositions.SWITCH_HEIGHT && isAtPosition(ElevatorPositions.SWITCH_HEIGHT)) {
-            return true;
-        }
-        return false;
+        return isAtPosition(targetPosition) && targetPosition != ElevatorPositions.GROUND_HEIGHT;
     }
 
     public double determineMotorSpeed(double currentPosition, double desiredPosition) {
@@ -95,37 +81,12 @@ public class ElevatorSystem {
 
         double percentOfDistanceRenaming = (distanceRemaining / desiredPosition);
         if (percentOfDistanceRenaming > stepDownStartAt) {
-            return Math.min(1,throttledMotorSpeed);
+            return Math.min(1, throttledMotorSpeed);
         }
 
         int excludedTravel = (int) Math.floor(desiredPosition * ((double) 1 - stepDownStartAt));
-        double motorSpeed =  distanceRemaining / (desiredPosition - excludedTravel);
-        return Math.min(motorSpeed,throttledMotorSpeed);
-    }
-
-    private void goToScale() {
-        double currentHeight = robotComponents.elevatorEncoder.getDistance();
-        if (currentHeight < scaleHeight) {
-            double motorSpeed = determineMotorSpeed(currentHeight, scaleHeight);
-            robotComponents.elevatorMotor.set(motorSpeed);
-            return;
-        }
-        robotComponents.elevatorMotor.set(0);
-    }
-
-    private void goToSwitch() {
-        double currentHeight = robotComponents.elevatorEncoder.getDistance();
-        if (currentHeight > switchHeight + switchRange) {
-            robotComponents.elevatorMotor.set(decentSpeed);
-            return;
-        }
-        if (currentHeight < switchHeight - switchRange) {
-            double motorSpeed = determineMotorSpeed(currentHeight, switchHeight);
-            robotComponents.elevatorMotor.set(motorSpeed);
-            return;
-        }
-
-        robotComponents.elevatorMotor.set(0);
+        double motorSpeed = distanceRemaining / (desiredPosition - excludedTravel);
+        return Math.min(motorSpeed, throttledMotorSpeed);
     }
 
     private void setStateFromController() {
